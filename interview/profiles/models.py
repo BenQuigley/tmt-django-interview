cc311e3a3d83a5eb76a534cbf58fde215bf4d256
Ben Quigley <ben.quigley.all@gmail.com>
2024-11-06 14:13:24 -0500

    Custom profiles table

--- interview/profiles/models.py
+++ interview/profiles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)  # hashed and salted
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField()
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
