3ad31c142c4efb96e27515bd58a53bde29b2743e
Ben Quigley <ben.quigley.all@gmail.com>
2024-11-06 13:47:01 -0500

    Flesh out the orders API; add deactivation

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import (OrderListCreateView,
+                                   OrderRetrieveUpdateDeactivateView,
+                                   OrderTagListCreateView)
 
 urlpatterns = [
     path("tags/", OrderTagListCreateView.as_view(), name="order-detail"),
+    path(
+        "<int:id>/",
+        OrderRetrieveUpdateDeactivateView.as_view(),
+        name="order-deactivate",
+    ),
     path("", OrderListCreateView.as_view(), name="order-list"),
 ]
--- interview/order/views.py
+++ interview/order/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
 from rest_framework import generics
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 # Create your views here.
@@ -13,3 +16,34 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+
+class OrderRetrieveUpdateDeactivateView(APIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+    def get(self, request: Request, *args, **kwargs) -> Response:
+        order = self.get_queryset(id=kwargs["id"])
+        serializer = self.serializer_class(order)
+
+        return Response(serializer.data, status=200)
+
+    def patch(self, request: Request, *args, **kwargs) -> Response:
+        order = self.get_queryset(id=kwargs["id"])
+        serializer = self.serializer_class(order, data=request.data, partial=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+
+        serializer.save()
+
+        return Response(serializer.data, status=200)
+
+    def delete(self, request: Request, *args, **kwargs) -> Response:
+        """Use DELETE to deactivate an order."""
+        order = self.get_queryset(id=kwargs["id"])
+        order.is_active = False
+        order.save()
+        return Response(status=204)
+
+    def get_queryset(self, **kwargs):
+        return self.queryset.get(**kwargs)
